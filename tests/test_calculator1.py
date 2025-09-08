d9ee4ea7dda47159098be369a3241c5abe736cf6
Ankush Chatterjee <ac.ankush15@gmail.com>
2025-09-08 16:40:25 +0530

    Update test_calculator1.py

--- tests/test_calculator1.py
+++ tests/test_calculator1.py
@@ -4,6 +4,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 import pytest
 from calculator12 import add, subtract, multiply, divide
 
+# dummy
 def test_add():
     assert add(2, 3) == 5
 
