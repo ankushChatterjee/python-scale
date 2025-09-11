37a47d7f1b80bcacf1a3f2c55f188f36b7eadc3d
Ankush Chatterjee <ac.ankush15@gmail.com>
2025-09-11 19:34:36 +0530

    Update test_calculator1.py

--- tests/test_calculator1.py
+++ tests/test_calculator1.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pytest
-from calculator12 import add, subtract, multiply, divide
+from calculator26 import add, subtract, multiply, divide
 
 # dummy2344
 def test_add():
